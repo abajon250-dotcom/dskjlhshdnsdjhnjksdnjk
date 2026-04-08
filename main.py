742e528defcb0e2971bb66806ceee0a5660c420d
abajon250-dotcom <abajon250@gmail.com>
2026-04-08 23:50:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import logging
 from aiogram import Bot
 from config import BOT_TOKEN
 from handlers import dp
-from models import init_db
 
 async def main():
     logging.basicConfig(level=logging.INFO)
     bot = Bot(token=BOT_TOKEN)
-    init_db()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
